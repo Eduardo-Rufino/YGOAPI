cffa247780ee69c31ae46b2b36680d555c798ce9
eduardo rufino <barceloseduardo@hotmail.com>
2026-05-06 01:02:53 -0300

    alteracao de CORS

--- api/YGOApi/Program.cs
+++ api/YGOApi/Program.cs
@@ -67,11 +67,18 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Configure CORS to allow requests from the frontend running on http://localhost:3000
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowLocal3000",
-        policy => policy.WithOrigins("http://localhost:3000")
+    options.AddPolicy("AllowFrontend",
+        policy => policy.WithOrigins(
+                            "http://localhost:3000", // Para seus testes locais
+                            "https://ygoapi-gamma.vercel.app", // Seu domínio principal na Vercel
+                            "https://ygoapi.vercel.app" // Um alias comum que a Vercel cria
+                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod());
 });
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -93,7 +100,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // Enable CORS using the configured policy
-app.UseCors("AllowLocal3000");
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
